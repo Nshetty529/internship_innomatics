2f5d9088397feb789cdbd88a8e5434917b04cd34
Nshetty529 <143995354+Nshetty529@users.noreply.github.com>
2024-09-14 15:43:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Search from './Search';
 function App() {
   return (
     <div className="App">
-      <h1>LOOK UP THE CAPITAL OR COUNTRY NAME</h1>
+      <h1>LOOK UP THE COUNTRY OR CAPITAL NAME</h1>
       <Search />
     </div>
   );
